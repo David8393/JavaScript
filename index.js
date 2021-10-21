cb1d391e497ebe69e76a27c01b01d3db0af0342c
David8393 <jo974586david@gmail.com>
2021-10-21 22:14:09 +0500

    test

--- index.js
+++ index.js
@@ -1,148 +1,178 @@
-//// 1-masala ////
+// 1-masala 
 
-// var a = 4;
-// var p = a * 4;
-// console.log(p)
+var a = 4;
+var p = a * 4;
+console.log(p)
 
-//// 2-masala ////
+// 2-masala 
 
-// var a = 3;
-// var s = a * a;
-// console.log(s)
+var a = 3;
+var s = a * a;
+console.log(s)
 
-//// 3-masala ////
+// 3-masala 
 
-// var a = 5;
-// var b = 7;
-// var s = a * b;
-// var p = 2 * (a + b);
-// console.log(s, p)
+var a = 5;
+var b = 7;
+var s = a * b;
+var p = 2 * (a + b);
+console.log(s, p)
 
-//// 4 - masala ////
+// 4 - masala 
 
-// var d = 7;
-// var p = 3.14;
-// var L = p * d;
-// console.log(L)
+var d = 7;
+var p = 3.14;
+var L = p * d;
+console.log(L)
 
-//// 5 - masala ////
+// 5 - masala 
 
-// var a = 5;
-// var v = a * a * a;
-// var s = 6 * (a ** 2);
-// console.log(v, s)
+var a = 5;
+var v = a * a * a;
+var s = 6 * (a ** 2);
+console.log(v, s)
 
-////    6 - masala    ////
+//    6 - masala
+var a = 5;
+var b = 3;
+var c = 3;
+var v = a * b * c;
+var s = 2 * (a * b + b * c + a * c)
+console.log(v, s)
 
-// var a = 5;
-// var b = 3;
-// var c = 3;
-// var v = a * b * c;
-// var s = 2 * (a * b + b * c + a * c)
-// console.log(v, s)
+//    7 - masala
 
-////    7 - masala    ////
+var r = 5;
+var p = 3.14
+var L = 2 * p * r;
+var S = p * (r ** 2)
+console.log(L, S)
 
-// var r = 5;
-// var p = 3.14
-// var L = 2 * p * r;
-// var S = p * (r ** 2)
-// console.log(L, S)
-
-////    8 - masala    ////
-// var a = 6;
-// var b = 7;
-// var s = (a + b) / 2;
-// console.log(s)
+//    8 - masala 
+var a = 6;
+var b = 7;
+var s = (a + b) / 2;
+console.log(s)
 
 
-////    9 - masala    ////
+//    9 - masala
+
+var a = 4;
+var b = 9;
+var c = ((a * b) ** 0.5 );
+console.log(c)
 
 ////     10 - masala    ////
 
-// var a = 5;
-// var b = 7;
-// var s = a + b;
-// var c = a * b;
-// var k1 = a * a;
-// var k2 = b * b;
-// console.log(s, c, k1, k2)
+var a = 5;
+var b = 7;
+var s = a + b;
+var c = a * b;
+var k1 = a * a;
+var k2 = b * b;
+console.log(s, c, k1, k2)
 
-////   11 - masala  ////
+//   11 - masala //
 
-////      13 - masala   ////
+//   12 - masala
 
-// var R1 = 5;
-// var R2 = 3;
-// var P = 3.14;
-// var S1 = P * R1;
-// var S2 = P * R2;
-// var S3 = P * (R1 - R2);
-// console.log(S1 , S2 , S3)
+var a = 5;
+var b = 12;
+var c = (((a ** 2) + (b ** 2)) ** 0.5);
+var P = a + b + c;
+console.log(c, P)
 
-////   14 - masala   ////
-// var d = 20;
-// var R = d / 2;
-// var P = 3.14;
-// var L = 2 * P * R;
-// var S = P * (R ** 2);
-// console.log(L , S)
+//      13 - masala
 
-////   15 - masala   ////
+var R1 = 5;
+var R2 = 3;
+var P = 3.14;
+var S1 = P * R1;
+var S2 = P * R2;
+var S3 = P * (R1 - R2);
+console.log(S1 , S2 , S3)
 
-// var d = 10;
-// var R = 5;
-// var P = 3.14;
-// var L = 2 * P * R;
-// var s = P * (R ** 2);
-// console.log(L , s)
+//   14 - masala
+var d = 20;
+var R = d / 2;
+var P = 3.14;
+var L = 2 * P * R;
+var S = P * (R ** 2);
+console.log(L , S)
 
-////   16 - masala   ////
+//   15 - masala
+
+var d = 10;
+var R = 5;
+var P = 3.14;
+var L = 2 * P * R;
+var s = P * (R ** 2);
+console.log(L , s)
+
+//   16 - masala //
 
 ////   17 - masala ////
 
-// var a = 7;
-// var b = 11;
-// var c = 21;
-// var y = b - a;
-// var x = c - b;
-// var z = x + y;
-// console.log(z)
+var a = 7;
+var b = 11;
+var c = 21;
+var y = b - a;
+var x = c - b;
+var z = x + y;
+console.log(z)
 
-////   18 - masala  ////
+//   18 - masala 
 
-// var a = 7;
-// var b = 11;
-// var c = 21;
-// var y = b - a;
-// var x = c - b;
-// var z = x * y;
-// console.log(z);
+var a = 7;
+var b = 11;
+var c = 21;
+var y = b - a;
+var x = c - b;
+var z = x * y;
+console.log(z);
 
-////   19- masala  ////
+//   19- masala 
 
-// var a = 4;
-// var b = 5;
-// var p = 2 * (a + b);
-// var s = a * b;
-// console.log(p, s)
+var a = 4;
+var b = 5;
+var p = 2 * (a + b);
+var s = a * b;
+console.log(p, s)
 
-////   22- masala ////
+//   20 - masala
 
-// var a = 12;
-// var b = 18;
-// var c = b;
-// b = a;
-// a = c;
-// console.log( a , b )
+var x2 = 5;
+var x1 = 1;
+var y2 = 4;
+var y1 = 1;
+var a = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5;
+console.log(a)
 
-////   23 - masala   ////
+//   21 - masala
 
-// var a = 10;
-// var b = 15;
-// var c = 20;
-// var d = a;
-// a = b;
-// b = c;
-// c = d;
-// console.log(a , b , c)
+var a = 5;
+var b = 12;
+var c = 13;
+var p = (a + b + c) / 2;
+var s = (p * (p - a) * (p - b) * (p - c)) ** 0.5;
+console.log(p, s)
+
+//   22- masala
+
+var a = 12;
+var b = 18;
+var c = b;
+b = a;
+a = c;
+console.log( a , b )
+
+//   23 - masala
+
+var a = 10;
+var b = 15;
+var c = 20;
+var d = a;
+a = b;
+b = c;
+c = d;
+console.log(a , b , c)
