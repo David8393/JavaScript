b8070a84bb69284dbd49240be637f0191af5bdfe
David8393 <jo974586david@gmail.com>
2021-10-21 13:37:50 +0500

    JS exercise

---
+++ index.js
@@ -0,0 +1,95 @@
+//// 1-masala ////
+
+// var a = 4;
+// var p = a * 4;
+// console.log(p)
+
+//// 2-masala ////
+
+// var a = 3;
+// var s = a * a;
+// console.log(s)
+
+//// 3-masala ////
+
+// var a = 5;
+// var b = 7;
+// var s = a * b;
+// var p = 2 * (a + b);
+// console.log(s, p)
+
+//// 4 - masala ////
+
+// var d = 7;
+// var p = 3.14;
+// var L = p * d;
+// console.log(L)
+
+//// 5 - masala ////
+
+// var a = 5;
+// var v = a * a * a;
+// var s = 6 * (a ** 2);
+// console.log(v, s)
+
+////    6 - masala    ////
+
+// var a = 5;
+// var b = 3;
+// var c = 3;
+// var v = a * b * c;
+// var s = 2 * (a * b + b * c + a * c)
+// console.log(v, s)
+
+////    7 - masala    ////
+
+// var r = 5;
+// var p = 3.14
+// var L = 2 * p * r;
+// var S = p * (r ** 2)
+// console.log(L, S)
+
+////    8 - masala    ////
+// var a = 6;
+// var b = 7;
+// var s = (a + b) / 2;
+// console.log(s)
+
+
+// 9 - masala
+
+////     10 - masala    ////
+
+// var a = 5;
+// var b = 7;
+// var s = a + b;
+// var c = a * b;
+// var k1 = a * a;
+// var k2 = b * b;
+// console.log(s, c, k1, k2)
+
+
+////      13 - masala   ////
+
+// var R1 = 5;
+// var R2 = 3;
+// var P = 3.14;
+// var S1 = P * R1;
+// var S2 = P * R2;
+// var S3 = P * (R1 - R2);
+// console.log(S1 , S2 , S3)
+
+////   14 - masala   ////
+
+// var R = 10;
+// var P = 3.14;
+// var L = 2 * P * R;
+// var S = P * (R ** 2);
+// console.log(L , S)
+
+////   15 - masala   ////
+
+var d = 10;
+var R = 5;
+var L = 
+ 
